035fd622ba66068254ee7f4adce3e4e77f868b53
Blassel228 <hok.ykrop@gmail.com>
2025-06-09 11:51:08 +0300

    api -api refactored to take limit and offset as arguments

--- src/Api/apiRoom.tsx
+++ src/Api/apiRoom.tsx
@@ -1,8 +1,12 @@
 import baseApi from "./apiBase.tsx";
 import { Room } from "../Types/types.tsx";
 
-export const getRooms = async (): Promise<Room[]> => {
-  const { data }: { data: Room[] } =
-    await baseApi.get<Room[]>(`/room`);
+export const getRooms = async (
+  offset?: number,
+  limit?: number
+): Promise<Room[]> => {
+  const { data }:{data: Room[]} = await baseApi.get<Room[]>("/room", {
+    params: { offset, limit },
+  });
   return data;
 };
