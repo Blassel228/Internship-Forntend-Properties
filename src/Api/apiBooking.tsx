6e1aafff4f22d54f557ae7bdb431b0aaca25915d
Blassel228 <hok.ykrop@gmail.com>
2025-09-19 11:14:22 +0300

    unused endpoints booking calls deleted

--- src/Api/apiBooking.tsx
+++ src/Api/apiBooking.tsx
@@ -1,31 +1,5 @@
 import baseApi from "./apiBase.tsx";
 import {Booking, BookingCreateIn} from "../Types/Booking.tsx";
-import {GuestCreateIn} from "../Types/Guest.tsx";
-import {Room} from "../Types/Room.tsx";
-
-export const createBookingWithToken = async (
-  bookingIn: BookingCreateIn,
-  guestIn: GuestCreateIn,
-): Promise<Booking> => {
-  const { data }: { data: Booking } = await baseApi.post<Booking>(
-    `/booking/create_booking_with_token`,
-    { booking_in: bookingIn, guest_in: guestIn },
-  );
-
-  return data;
-};
-
-export const createBookingWithoutToken = async (
-  bookingIn: BookingCreateIn,
-  guestIn: GuestCreateIn,
-): Promise<Booking> => {
-  const { data }: { data: Booking } = await baseApi.post<Booking>(
-    `/booking/create_booking_without_token`,
-    { booking_in: bookingIn, guest_in: guestIn },
-  );
-
-  return data;
-};
 
 export const getBookingsForOneUser = async (): Promise<Booking[]> => {
   const { data } : { data: Booking[] }  = await baseApi.get('/booking/get_bookings_for_one_user');
